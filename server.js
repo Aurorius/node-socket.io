8d95ebaa96bb1ea9e619063358dea3a584435e77
Aurorius <62819094+Aurorius@users.noreply.github.com>
2020-11-18 22:59:11 +0300

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,11 @@ const PORT = process.env.PORT || 3000;
 const INDEX = '/index.html';
 
 const server = express()
-  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+
+  .use('/test', function (req, res, next) {
+  res.sendFile(INDEX, { root: __dirname })
+  next()
+  })
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
 const io = socketIO(server);
